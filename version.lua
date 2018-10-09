e8265df1024656309dd3b9d0bd07bb59a5138727
Jarrett B <jarrettboice@gmail.com>
2018-10-09 15:42:11 -0700

    Another version update

--- version.lua
+++ version.lua
@@ -1,7 +1,6 @@
 --[[ Version Checker ]]--
 local version = 1.3
 
-
 AddEventHandler("onResourceStart", function(resource)
     if resource == GetCurrentResourceName() then
         CheckFrameworkVersion()
@@ -10,19 +9,20 @@ end)
 
 function CheckFrameworkVersion()
     PerformHttpRequest("https://raw.githubusercontent.com/itsJarrett/FiveM-boot_vehicles/master/version.txt", function(err, text, headers)
-
         if text == tostring(version) then
             print(" ")
             print("---------- BOOT VEHICLES VERSION ----------")
             print("Boot Vehicles Script Updated and ready to go!")
             print("https://github.com/itsJarrett/FiveM-boot_vehicles")
             print("--------------------------------")
+            print(" ")
         else
             print(" ")
             print("---------- BOOT VEHICLES VERSION ----------")
             print("Boot Vehicles Script is outdated. Update for more features!")
             print("https://github.com/itsJarrett/FiveM-boot_vehicles")
             print("--------------------------------")
+            print(" ")
         end
 
     end, "GET", "", {})
